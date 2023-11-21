1c039325f130a6aa9413123c7345b1113a026fdc
Kapil <kapiljavin@gmail.com>
2023-11-21 19:19:00 +0530

    Updated

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export class Log {
+    static success(msg: string){
+        console.log(`%c ${msg}`, 'color: green')
+    }
+    static danger(msg: string){
+        console.log(`%c ${msg}`, 'color: red')
+
+    }
+    static info(msg: string){
+        console.log(`%c ${msg}`, 'color: #000; background: yellow')
+    }
+}
